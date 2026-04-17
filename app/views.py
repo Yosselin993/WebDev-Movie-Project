b93d051173f7be6168fcf35c2774b6aebb509d1b
Yosselin <yosselinmeza993@gmail.com>
2026-04-16 21:07:07 -0500

    commiting setup

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def home(request):
+    context = {}
+    template = loader.get_template("index.html");
+    return HttpResponse(template.render(context, request))
+
+def moviesingle(request):
+    context = {}
+    template = loader.get_template("moviesingle.html");
+    return HttpResponse(template.render(context, request))
+
+    
