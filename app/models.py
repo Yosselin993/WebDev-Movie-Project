eb3a3085bd66a82bc145555f4fb59d44762aec2e
Yosselin <yosselinmeza993@gmail.com>
2026-04-20 19:52:09 -0500

    Added base.html, connected app in settings, added two classes but have not implemented into code yet

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class SocialLink(models.Model):
+    #idk about the ID field
+    name = models.CharField(max_length=50)
+    anchor_class = models.CharField(max_length=2)
+    icon_class = models.CharField(max_length=30)
+    url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name;
+
+class Trailer(models.Model):
+    #idk about the ID field
+    trailer_URL = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.trailer_URL;
